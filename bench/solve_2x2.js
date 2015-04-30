6a5551172d5d600f2938ea89d133145b875d77ab
Justin Jaffray <justin.jaffray@gmail.com>
2015-04-30 22:35:32 +0100

    Some optimizations + benchmark

---
+++ bench/solve_2x2.js
@@ -0,0 +1,12 @@
+var puz = require("../lib/two");
+var solve = require("../lib/solve");
+
+for (var i = 0; i < 100; i++) {
+  solve(puz, {
+    perm: [0, 2, 1, 3, 4, 5, 6],
+    orie: {
+      orie: [0, 2, 1, 0, 0, 0, 0],
+      perm: [0, 1, 2, 3, 4, 5, 6]
+    }
+  });
+}
