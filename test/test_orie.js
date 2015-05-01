8502fcdc8e2859cdb8e2fef60a7e9e7461aa1705
Justin Jaffray <justin.jaffray@gmail.com>
2015-05-01 12:57:37 +0100

    Fix tests

--- test/test_orie.js
+++ test/test_orie.js
@@ -7,7 +7,7 @@ var assert_throws_msg = require("./assert_throws_msg");
 describe("creating normally", function() {
   it("doesn't complain creating a valid orientation", function() {
     new Orie({
-      kind: Orie.kinds.ZERO_SUM,
+      kind: Orie.kinds.zero_sum,
       size: 7,
       order: 3,
       moveEffects: {}
@@ -20,7 +20,7 @@ describe("creating invalid", function() {
     // TODO: these error messages sort of suck, should either wrap js-schema or find something better.
     assert_throws_msg("{\"size\":\"undefined is not Number\"}", function() {
       new Orie({
-        kind: Orie.kinds.ARBITRARY_SUM,
+        kind: Orie.kinds.arbitrary_sum,
         // size: 7,
         order: 3,
         moveEffects: {}
@@ -31,7 +31,7 @@ describe("creating invalid", function() {
   it("throws an error if it has a move that doesn't have both perm and orie", function() {
     assert_throws_msg("Move 'F' missing perm.", function() {
       new Orie({
-        kind: Orie.kinds.ZERO_SUM,
+        kind: Orie.kinds.zero_sum,
         size: 7,
         order: 3,
         moveEffects: {
@@ -45,7 +45,7 @@ describe("creating invalid", function() {
 
     assert_throws_msg("Move 'F' missing orie.", function() {
       new Orie({
-        kind: Orie.kinds.ZERO_SUM,
+        kind: Orie.kinds.zero_sum,
         size: 7,
         order: 3,
         moveEffects: {
@@ -61,7 +61,7 @@ describe("creating invalid", function() {
   it("throws an error if it has a move whose perm or orie is the wrong length", function() {
     assert_throws_msg("Orie on orientation move 'F' had length 6, expected length 7.", function() {
       new Orie({
-        kind: Orie.kinds.ZERO_SUM,
+        kind: Orie.kinds.zero_sum,
         size: 7,
         order: 3,
         moveEffects: {
@@ -75,7 +75,7 @@ describe("creating invalid", function() {
 
     assert_throws_msg("Perm move for move named 'F' had length 6, expected length 7.", function() {
       new Orie({
-        kind: Orie.kinds.ZERO_SUM,
+        kind: Orie.kinds.zero_sum,
         size: 7,
         order: 3,
         moveEffects: {
@@ -91,7 +91,7 @@ describe("creating invalid", function() {
   it("verifies the ories and perms of a move are valid", function() {
     assert_throws_msg("[3, 0, 0, 0, 0, 0, 0] is not a valid orientation for an orientation with order 3.", function() {
       new Orie({
-        kind: Orie.kinds.ZERO_SUM,
+        kind: Orie.kinds.zero_sum,
         size: 7,
         order: 3,
         moveEffects: {
@@ -104,7 +104,7 @@ describe("creating invalid", function() {
     });
     assert_throws_msg("[0, 1, 2, 3, 4, 5, 5] is not a valid permutation.", function() {
       new Orie({
-        kind: Orie.kinds.ZERO_SUM,
+        kind: Orie.kinds.zero_sum,
         size: 7,
         order: 3,
         moveEffects: {
@@ -121,7 +121,7 @@ describe("creating invalid", function() {
     // this is a pretty cute error msg, probs not needed right off the bat, but would be sweet
     assert_throws_msg("Orie move definitions create a cycle: U -> F -> U.", function() {
       new Orie({
-        kind: Orie.kinds.ZERO_SUM,
+        kind: Orie.kinds.zero_sum,
         size: 7,
         order: 3,
         moveEffects: {
@@ -137,7 +137,7 @@ describe("creating invalid", function() {
   it.skip("does not allow moves defined as algs to be defined in terms of other moves defined as algs", function() {
     assert_throws_msg("Moves cannot be defined in terms of other moves if those moves are not explicitly defined", function() {
       new Orie({
-        kind: Orie.kinds.ZERO_SUM,
+        kind: Orie.kinds.zero_sum,
         size: 7,
         order: 3,
         moveEffects: {
